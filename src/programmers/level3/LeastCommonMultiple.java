2b2819698a00b908a69111f886b8feaa52354098
jinseok <yellownavy83@gmail.com>
2018-04-27 23:41:22 +0900

    [CREATE] - Java Study Project

---
+++ src/programmers/level3/LeastCommonMultiple.java
@@ -0,0 +1,17 @@
+package programmers.level3;
+
+public class LeastCommonMultiple {
+
+    public long nlcm(int[] num) {
+        long answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        LeastCommonMultiple c = new LeastCommonMultiple();
+        int[] ex = { 2, 6, 8, 14 };
+        // 아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println(c.nlcm(ex));
+    }
+}
